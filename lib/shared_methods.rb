c2446389a13ec632c8adfc0fcb46156571373048
Noah Berman <noah@noahberman.org>
2016-09-12 16:43:19 -0600

    initial shared methods

--- lib/shared_methods.rb
+++ lib/shared_methods.rb
@@ -1,7 +1,19 @@
+require 'csv'
+
 module SharedMethods
 
-  def load_data(contents_to_load = {})
+  def load_csv(contents_to_load = {})
+   @data = CSV.open contents_to_load, headers: true, header_converters: :symbol
   end
+  
+  def turn_csv_into_hash(filename_path = "./data/Kindergartners in full-day program.csv")
+    load_csv(filename_path)
+    data_to_parse = @data.map { |info| info }
+       
+  end
+
+
+
 
   def find_by_name(name = "")
   end
--- test/shared_methods_test.rb
+++ test/shared_methods_test.rb
@@ -4,3 +4,16 @@ require './lib/shared_methods'
 require 'pry'
 
 class SharedMethodsTest < Minitest::Test
+  include SharedMethods
+
+  def test_loader_can_load_CSV_files
+    assert_instance_of CSV, load_csv('./data/Kindergartners in full-day program.csv')
+  end
+
+  def test_loader_can_create_array
+    assert_instance_of Array, turn_csv_into_hash
+  end
+  
+  def test_loader
+
+end
