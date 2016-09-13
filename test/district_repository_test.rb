c2a0735b9c5aeb9d7a3adbd5aaacc6a9e03a8570
Noah Berman <noah@noahberman.org>
2016-09-13 13:57:43 -0600

    fixed find a single district test

--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -7,26 +7,15 @@ class DistrictRepositoryTest < Minitest::Test
 
   def test_dr_can_load_a_csv
     dr = DistrictRepository.new
-
-    dr.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"
-      }
-    })
-    binding.pry
     assert_instance_of Hash, dr.data_hash
   end
   
   def test_dr_can_find_a_district_by_name
     
     dr = DistrictRepository.new
-    dr.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"
-      }
-    })
     dr.find_by_name("ACADEMY 20")
     assert_equal "0.49022", dr.district["data"]
+  
   end
   
   def test_dr_can_find_using_just_a_fragment
