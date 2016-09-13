419c3bb95ee6a1e59f2af5c14159d309e13daedf
Noah Berman <noah@noahberman.org>
2016-09-12 18:56:48 -0600

    minor fixes plus load_data and test

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -2,9 +2,7 @@ require 'csv'
 require 'pry'
 class DistrictRepository
 
-  def load_data(contents_to_load = {})
-    data = CSV.open contents_to_load
-  end
+
 
   def find_by_name
   end
--- lib/shared_methods.rb
+++ lib/shared_methods.rb
@@ -2,33 +2,32 @@ require 'csv'
 
 module SharedMethods
 
-  def load_csv(contents_to_load = {})
-   @data = CSV.open contents_to_load, headers: true, header_converters: :symbol
+  def load_csv(filename_path)
+   @data = CSV.open filename_path, headers: true, header_converters: :symbol
   end
 
-  def turn_csv_into_array(filename_path = "./data/Kindergartners in full-day program.csv")
+  def turn_csv_into_array(filename_path)
     load_csv(filename_path)
     data_to_parse = @data.map { |info| info }
   end
 
-  def hash_populate
-    load_csv("./data/Kindergartners in full-day program.csv")
+  def hash_populate(filename_path)
+    load_csv(filename_path)
     data_hash = {}
     @data.each_with_index do |row, i|
     location = row[:location]
     timeframe = row[:timeframe]
     dataformat = row[:dataformat]
     data = row[:data]
-    data_hash["location" => location]  =
+    data_hash[location]  =
                     {"timeframe"    => timeframe,
-                    "dataformat" => dataformat,
                     "data"       => data}
                   end
     data_hash
   end
-
-
-  def find_by_name(name = "")
+  
+  def load_data(filename_path)
+    hash_populate(filename_path)
   end
 
 end
