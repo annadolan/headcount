19a62970fd02f652975a9ff23342f023d82d00f5
Noah Berman <noah@noahberman.org>
2016-09-14 21:02:38 -0600

    working first part of iteration 1

--- lib/district.rb
+++ lib/district.rb
@@ -1,5 +1,5 @@
-require_relative 'shared_methods'
 require_relative 'enrollment_repository'
+require_relative 'shared_methods'
 require_relative 'kindergarten'
 
 class District
@@ -7,11 +7,11 @@ class District
   include SharedMethods
   include Kindergarten
   
-  attr_reader :name, :enrollment, :district_name
+  attr_reader :name, :enrollment, :district_name, :enrollment_data
  
   def initialize(name_and_enrollment_data)
     @name = name_and_enrollment_data[:name]
-    @enrollment = name_and_enrollment_data[:kindergarten_participation]
+    @enrollment = Enrollment.new(name_and_enrollment_data)
   end
 
 
