8ce71b5f2028b11e5202675fe2ded47fd68f3905
Noah Berman <noah@noahberman.org>
2016-09-17 19:44:51 -0600

    sanitation test passing for iterations zero and one

--- lib/shared_methods.rb
+++ lib/shared_methods.rb
@@ -4,17 +4,21 @@ module SharedMethods
   attr_reader :organized_entries, :enrollment
 
   def load_csv(path, key)
-    e = []
-    CSV.foreach(path, headers: true, header_converters: :symbol) do |i|
-      e << ({:name => i[:location].upcase, i[:timeframe].to_i => i[:data].to_f})
+    enroll_array = []
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |row|
+      location = row[:location].upcase
+      year = row[:timeframe].to_i
+      data = row[:data].to_f
+      enroll_array << ({:name => location, year => data})
     end
-    parse(e, key)
+    parse(enroll_array, key)
   end
 
    def parse(enroll_array, key)
-     temp_array = enroll_array.group_by { |item| item.values.first }.map{|_, second| second.reduce(:merge)}
-     parse = temp_array.reduce({}) do |result, item|
-      temp_array.map do |item|
+     temp_array = enroll_array.group_by { |item| item.values.first }
+     fix_array = temp_array.map{|_, second| second.reduce(:merge)}
+     parse = fix_array.reduce({}) do |result, item|
+      fix_array.map do |item|
       {:name => item.values_at(:name).join, key => item}
       end
     end
@@ -29,8 +33,8 @@ module SharedMethods
    end
 
    def zip_arrays(kindergarten_array, hs_array)
-     all_enrollment = kindergarten_array.zip(hs_array).map do |kindergarten|
-       kindergarten.reduce(&:merge)
+     all_enrollment = kindergarten_array.zip(hs_array).map do |array|
+       kindergarten_array.reduce(&:merge)
 
      end
      new_enrollment(all_enrollment)
