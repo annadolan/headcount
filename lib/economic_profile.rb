080672c64b985c49054e89366c7a1b1f3f184c46
Noah Berman <noah@noahberman.org>
2016-09-21 13:58:07 -0600

    fixes errors, creates new ones

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -15,24 +15,45 @@ class EconomicProfile
           2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]
 
   def initialize(data)
-    @data = {}
+    @data = data
   end
 
 
   def median_household_income_in_year(year)
     raise UnknownDataError unless YEARS.include?(year)
-    nums_to_average = []
-    result = data[0][:median_household_income].map do |row|
-      if row.values[0].keys[0].to_s.include?(year.to_s)
-        nums_to_average << row.values[0].values[0].to_i
+    income = []
+    result = data[:median_household_income].map do |row|
+      if is_year_between?(year, row) == true
+        income << row[1].to_i
+      else
+        nil
       end
+      income
     end
-    result = nums_to_average.reduce(:+) / nums_to_average.count
+    result = income.reduce(:+) / income.count
   end
+  
+  def is_year_between?(year, row)
+    if row[0].class == Array
+      range = row[0]
+    else
+      range = row[0].split("-")
+    end
+    temp_range = range.map do |date|
+      date.to_i
+    end
+    temp_range
+    year.between?(temp_range[0], temp_range[1])
+  end
+    
 
   def median_household_income_average
-    household_income_avg = data[0][:median_household_income].map do |row|
-      row.values[0].values[0].to_i
+    if data.class == Hash
+      household_income_avg = data[:median_household_income].values
+    else
+      household_income_avg = data[0][:median_household_income].map do |row|
+        row.values[0].values[0].to_i
+      end
     end
     result = household_income_avg.reduce(:+) / household_income_avg.count
     result
@@ -41,15 +62,18 @@ class EconomicProfile
   def children_in_poverty_in_year(year)
     raise UnknownDataError unless YEARS.include?(year)
     percent = []
-    array_to_check = data[1][:children_in_poverty]
+    if data.class == Hash
+      array_to_check = data[:children_in_poverty].values
+    else
+      array_to_check = data[1][:children_in_poverty]
+    end
     result = array_to_check.map do |row|
       if row.values[0].keys[0].to_s.include?(year.to_s)
         percent << truncate_float(row.values[0].values[0].to_f)
     
       end
     end
-    percent.sort! # this is because until we get the number/percent thing figured out, this should return
-                  # the right answer. urhg urhg urhg
+    percent.sort!
     final_answer = percent[0]
   end
   
